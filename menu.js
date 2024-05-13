d4cc7025d1ff1b909c53e5dd9c40d97b17a583f6
Alfakynz <141228129+Alfakynz@users.noreply.github.com>
2024-05-13 16:04:11 +0200

    Bug fixes
    Change the script to search heroes
    Fix css
    Add Thor in web structure

--- menu.js
+++ menu.js
@@ -1,16 +1,27 @@
 // Fonction pour rechercher des héros
 function search() {
-    var input = document.querySelector('input[type="text"]').value.toLowerCase();
+    var input = document.querySelector('input[type="text"]').value.trim().toLowerCase();
 
-    var heroes = document.querySelectorAll('#menu-list section');
-
-    heroes.forEach(function(hero) {
-        var name = hero.textContent.toLowerCase();
-
-        if (name.includes(input)) {
-            hero.style.display = 'block';
-        } else {
-            hero.style.display = 'none';
+    var heroes = [
+        {
+            id: 'tony-stark',
+            name: 'Tony Stark',
+            alias: 'Iron Man'
+        },
+        {
+            id: 'bruce-banner',
+            name: 'Bruce Banner',
+            alias: 'Hulk'
+        },
+        {
+            id: 'thor',
+            name: 'Thor'
         }
+    ];
+
+    heroes.forEach(hero => {
+        var nameID = document.getElementById(hero.id);
+        var found = hero.name.toLowerCase().includes(input) || (hero.alias && hero.alias.toLowerCase().includes(input));
+        nameID.style.display = found ? 'block' : 'none';
     });
 }
