ffdef8124f14b61ec967a0169b3729939ca6070a
Alfakynz <141228129+Alfakynz@users.noreply.github.com>
2024-05-15 19:02:17 +0200

    Add videos for Thor + update videos's reading
    Add script to pause video when a new video is clicked
    Add videos for Thor

---
+++ scripts/video.js
@@ -0,0 +1,15 @@
+const videos = document.querySelectorAll('video');
+
+videos.forEach(video => {
+    video.addEventListener('play', function() {
+        pauseOtherVideos(video);
+    });
+});
+
+function pauseOtherVideos(currentVideo) {
+    videos.forEach(video => {
+        if (video !== currentVideo && !video.paused) {
+            video.pause();
+        }
+    });
+}
